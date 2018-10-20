5eff0c4c9c0dd03843cddd16d5edd1e07c48aedc
kevingamaa <vitorkortez@gmail.com>
2018-10-20 16:40:48 -0300

    update

--- view/home.php
+++ view/home.php
@@ -50,26 +50,81 @@
 		</div>
 
 	</header>
-	<div class="container-fluid">
+
+	<div class="container-fluid p-4  bg-info">
 		<div class="row">
-			<div class="col-md-12 text-right">
-				<a class="btn btn-outline-light btn-lg"> href="<?=pasta?>">
-					
+			<div class="col-md-7 text-white">
+				O CCA depende de voluntários para suas atividades. Caso tenha interesse envie o seu contato com sua área de experiencia aqui. Ou ligue para 5686-3288 às 6as feiras das 8:00 as 12:00.
+			</div>
+			<div class="col-md-5 text-center">
+				<a class="btn btn-outline-light btn-lg" href="/<?=pasta?>/doacoes">
+					<h1 class="h5">Se voluntariar</h1>
 				</a>
 			</div>
 		</div>
 	</div>
-	<div class="card">
-		<div class="card-header">
-			<h4 class="card-title">Titulos</h4>
-		</div>
-		<div class="card-body">
-			
-		</div>
-		<div class="card-footer">
-			
+
+	<div class="container-fluid">
+		<div class="card">
+			<div class="card-header text-center">
+				<h4 class="card-title">ONG - Casa das Crianças </h4>
+				<hr width="100">
+			</div>
+			<div class="card-body">
+
+				<div class="row text-center">
+					<div class="col-md-4 shadow-lg">
+						<h4 class="h4">Quem Somos</h4>
+						<img src="/<?=pasta?>/asset/img/quem-somos-home.jpg">
+						<p class="text-justify pt-3 mt-1">
+							omos uma entidade sem fins lucrativos que atende crianças de 0 a 18 anos. 
+							Apoiamos o processo de desenvolvimento e formação de crianças e adolescentes atendidos em nossos três principais projetos – Casa Lar (Serviços de Acolhimento de Crianças e Adolescentes), Saica (Serviço de Acolhimento Institucional de Crianças e Adolescentes) e CCA (Contra Turno Escolar). 
+							Para isso, procuramos oferecer um ambiente acolhedor, baseado em respeito e carinho, que possibilite o desenvolvimento integral das crianças e adolescentes atendidos na Entidade.
+						</p>
+						<a href="/<?=pasta?>/quemsomos">
+							<small>leia mais...</small>
+						</a>
+					</div>
+
+				
+					<div class="col-md-4">
+						<h4 class="h4">Como Ajudar</h4>
+						<p class="text-justify pt-3 ">
+							Cadastre-se aqui para receber informações sobre nossos eventos.
+						</p>
+						<img src="/<?=pasta?>/asset/img/como-ajudar-home.jpg">
+						<p class="text-justify mt-1 pt-3">
+							Existem diversas formas de ajudar!
+							Aceitamos doações para apoiar os nossos projetos, <br> bem como doação de materiais (roupas, produtos de limpeza, alimentos, pedagógicos).
+						</p>
+						<p class="table-warning shadow">
+							A Casa da Criança esta precisando adequar suas instalações para melhor atender nossas crianças. Se você puder nos ajudar, por favor entre em contato.
+						</p>
+						<a href="/<?=pasta?>/doacoes">
+							<small>leia mais...</small>
+						</a>
+					</div>
+
+					<div class="col-md-4 shadow-lg">
+						<h4 class="h4">Projetos</h4>
+						<img src="/<?=pasta?>/asset/img/projetos-o.jpg">
+						<p class="text-justify pt-3 mt-1">
+							<strong> O Centro da Criança e do Adolescente </strong> - CCA atende 70 crianças e adolescentes ( 06 a 14 anos ) separadas em duas salas de aula por período e conta com educadores de diferentes linguagens
+						</p>
+						
+						<a href="/<?=pasta?>/projetos">
+							<small>leia mais...</small>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="card-footer">
+				
+			</div>
 		</div>
 	</div>
+	
+
 
 	<?php include_once('build/footer.php') ?>
 	<script type="text/javascript">
--- view/projetos.php
+++ view/projetos.php
@@ -30,7 +30,7 @@
 				<br>
 				<strong class="indiceNav">Como chegar:</strong><small>
 				<br>
-				<iframe style="width: 100%" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com.br/maps?f=q&amp;source=s_q&amp;hl=pt-br&amp;q=R.+Padre+Chico,+320+-+Santo+Amaro,+S%C3%A3o+Paulo,+04747-090&amp;sll=-14.179186,-50.449219&amp;sspn=91.125623,186.152344&amp;ie=UTF8&amp;cd=1&amp;geocode=FcEGl_4d53Y3_Q&amp;split=0&amp;hq=&amp;hnear=R.+Padre+Chico,+320+-+Santo+Amaro,+S%C3%A3o+Paulo,+04747-090&amp;ll=-23.656789,-46.696701&amp;spn=0.015724,0.023174&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
+				<iframe style="width: 100%" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  src="http://maps.google.com.br/maps?f=q&amp;source=s_q&amp;hl=pt-br&amp;q=R.+Padre+Chico,+320+-+Santo+Amaro,+S%C3%A3o+Paulo,+04747-090&amp;sll=-14.179186,-50.449219&amp;sspn=91.125623,186.152344&amp;ie=UTF8&amp;cd=1&amp;geocode=FcEGl_4d53Y3_Q&amp;split=0&amp;hq=&amp;hnear=R.+Padre+Chico,+320+-+Santo+Amaro,+S%C3%A3o+Paulo,+04747-090&amp;ll=-23.656789,-46.696701&amp;spn=0.015724,0.023174&amp;z=14&amp;iwloc=A&amp;output=embed" allowfullscreen></iframe>
 				
 
 
