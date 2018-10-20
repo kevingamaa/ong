0c0bba2e8148f33f0edeea55e8de0e706de25475
Guilhermegama <juninho12378@icloud.com>
2018-10-20 16:40:10 -0300

    update

--- view/projetos.php
+++ view/projetos.php
@@ -7,6 +7,12 @@
 </head>
 <body class="bg-light">
 	<?php include_once('build/navbar.php') ?>
+
+	<header id="headerprojetos">
+		
+
+	</header>
+
 	<div class="container">
 		<div class="row">
 			<div class="col-md-">
@@ -16,22 +22,21 @@
 				<h3>
 					Contato
 				</h3>
-				<small>
-					casa@casadacriancasantoamaro.org.br
-				</small>
-				<br>
+				<strong class="indiceNav">E-mail: </strong><small><a href="mailto:casa@casadacriancasantoamaro.org.br"> casa@casadacriancasantoamaro.org.br</a></small><br>
 				<strong class="indiceNav">Telefone: </strong><small>55 11 5686.3288 / 3624.2660</small><br>
 					<strong class="indiceNav">Fax: </strong><small>55 11 5686.3288</small><br>
 				<strong class="indiceNav">Endereço: </strong><small>Rua Padre chico, 320 - Santo Amaro - SP</small><br>
 				
-				<a href="doacoes" class="btn btn-block btn-outline-primary">
+				<a href="doacoes" class="btn btn-block btn-outline-primary mt-4">
 					FAÇA SUA DOAÇÃO
 				</a>
 				<br>
 				<strong class="indiceNav">Como chegar:</strong><small>
 				<br>
-				<iframe style="width: 100%" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com.br/maps?f=q&amp;source=s_q&amp;hl=pt-br&amp;q=R.+Padre+Chico,+320+-+Santo+Amaro,+S%C3%A3o+Paulo,+04747-090&amp;sll=-14.179186,-50.449219&amp;sspn=91.125623,186.152344&amp;ie=UTF8&amp;cd=1&amp;geocode=FcEGl_4d53Y3_Q&amp;split=0&amp;hq=&amp;hnear=R.+Padre+Chico,+320+-+Santo+Amaro,+S%C3%A3o+Paulo,+04747-090&amp;ll=-23.656789,-46.696701&amp;spn=0.015724,0.023174&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
-				
+				<iframe width="400" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com.br/maps?f=q&amp;source=s_q&amp;hl=pt-br&amp;q=R.+Padre+Chico,+320+-+Santo+Amaro,+S%C3%A3o+Paulo,+04747-090&amp;sll=-14.179186,-50.449219&amp;sspn=91.125623,186.152344&amp;ie=UTF8&amp;cd=1&amp;geocode=FcEGl_4d53Y3_Q&amp;split=0&amp;hq=&amp;hnear=R.+Padre+Chico,+320+-+Santo+Amaro,+S%C3%A3o+Paulo,+04747-090&amp;ll=-23.656789,-46.696701&amp;spn=0.015724,0.023174&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
+				<a href="http://www.redial.net.br/casadacrianca/content/8-recaridade" target="_blank">
+					<img src="/<?=pasta?>/asset/img/redial-net.jpg">
+				</a>
 
 
 			</aside>
