05e91c7712a5b49fd1627674b37f7baafdaf646a
Guilhermegama <juninho12378@icloud.com>
2018-10-20 15:35:41 -0300

    fix

---
+++ controller/ProjetosController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace controller;
+
+class Projetos{
+    public function index(){
+        include('view/projetos.php');
+    }
+
+    
+   
+}
---
+++ view/projetos.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<?php include_once('build/head.php') ?>
+	<title>ONG - Casa da criança</title>
+
+</head>
+<body class="bg-light">
+	<?php include_once('build/navbar.php') ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-">
+				
+			</div>			
+			<aside class="col-md-4 p-4">
+				<h3>
+					Contato
+				</h3>
+				<small>
+					casa@casadacriancasantoamaro.org.br
+				</small>
+				<br>
+				<strong class="indiceNav">Telefone: </strong><small>55 11 5686.3288 / 3624.2660</small><br>
+					<strong class="indiceNav">Fax: </strong><small>55 11 5686.3288</small><br>
+				<strong class="indiceNav">Endereço: </strong><small>Rua Padre chico, 320 - Santo Amaro - SP</small><br>
+				
+				<a href="doacoes" class="btn btn-block btn-outline-primary">
+					FAÇA SUA DOAÇÃO
+				</a>
+				<br>
+				<strong class="indiceNav">Como chegar:</strong><small>
+				<br>
+				<iframe width = "600" height = "450" frameborder = "0" style = "border: 0"  src = "https://www.google.com/maps/embed/v1/place?key= YOUR_API_KEY
+					    & q = espaço + agulha, Seattle + WA "allowfullscreen>
+					</ iframe>
+
+
+			</aside>
+		</div>
+	</div>
+
+	<?php include_once('build/footer.php') ?>
+</body>
+</html>
