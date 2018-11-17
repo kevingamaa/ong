3a58aa2be308ae0845191c5560c5cbb0ef8d3ccf
Guilhermegama <juninho12378@icloud.com>
2018-11-17 11:17:41 -0200

    2

--- build/footer.php
+++ build/footer.php
@@ -1,3 +1,31 @@
+<footer class="bg-dark shadow">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-4">
+				Casa da Criança e Adolescente Santo Amaro Grossarl Rua Padre Chico, 320 
+				Santo Amaro - São Paulo - SP / Cep: 04747-090 
+				Email: casadacriancacca@hotmail.com
+				Tel. 55 11 5686.3288 / 3624.2660
+				Fax. 55 11 5686.3288
+			</div>
+			<div class="col-md-4">
+				Registros Institucionais:
+				- Utilidade Pública Federal Portaria nº1026 de 09/01/01 Publ. DOE 12/11/01
+				- Utilidade Pública Federal Portaria n44.732 de 29/02/00 Publ. DOE 01/03/00
+			</div>
+			<div class="col-md-4">
+				SEXO3
+			</div>
+			<div class="col-md-4">
+				
+			</div>
+			
+		</div>
+		
+	</div>
+</footer>
+
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
--- view/projetos.php
+++ view/projetos.php
@@ -17,18 +17,13 @@
 	<div class="container-fluid">
 		<div class="row p-4">			
 			
-			<div class="col-md-8">
-				<div class="card" style="width: 18rem;">
-				  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-				  <div class="card-body">
-				    <h5 class="card-title">Card title</h5>
-				    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				    <a href="#" class="btn btn-primary">Go somewhere</a>
-				  </div>
-				</div>
+			<div class="col-md-9">
+				
 			</div>
 
-			<aside class="col-md-3 p-3 border border-secondary rounded " style="background-color: #f2f2f2">
+			
+
+			<aside class="col-md-2 p-3 border border-secondary rounded " style="background-color: #f2f2f2">
 				<h3>
 					Contato
 				</h3>
