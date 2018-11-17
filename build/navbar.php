7668c3ca6fdc4b21b8d776556a836eb88071f95b
kevingamaa <vitorkortez@gmail.com>
2018-11-17 14:05:57 -0200

    upando

---
+++ build/navbar.php
@@ -0,0 +1,16 @@
+<section id="fh5co-header">
+	<div class="container">
+		<nav role="navigation">
+			<ul class="pull-left left-menu">
+				<li><a href="about.html">About</a></li>
+				<li><a href="tour.html">Tour</a></li>
+				<li><a href="pricing.html">Pricing</a></li>
+			</ul>
+			<h1 id="fh5co-logo"><a href="index.html">guide<span>.</span></a></h1>
+			<ul class="pull-right right-menu">
+				<li><a href="#">Login</a></li>
+				<li class="fh5co-cta-btn"><a href="#">Sign up</a></li>
+			</ul>
+		</nav>
+	</div>
+</section>
