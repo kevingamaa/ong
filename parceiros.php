f42fe8b02060e34c634f97173e368f5549d26705
Guilhermegama <juninho12378@icloud.com>
2018-11-17 20:37:26 -0200

    upupupu

---
+++ parceiros.php
@@ -0,0 +1,293 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<?php
+			require_once('build/head.php');
+		?>
+		<title>Parceiros - Casa da criança e do adolescente </title>
+	</head>
+	<body>
+
+		<!-- Loader -->
+		<div class="fh5co-loader"></div>
+		
+		<div id="fh5co-page">
+			<?php
+				require_once('build/navbar.php');
+			?>
+			<section id="fh5co-hero" class="no-js-fullheight" style="background-image: url(img/850_400_diacriancas.jpg);" data-next="yes">
+				<div class="fh5co-overlay"></div>
+				<div class="container">
+					<div class="fh5co-intro no-js-fullheight">
+						<div class="fh5co-intro-text">
+							<!-- 
+								INFO:
+								Change the class to 'fh5co-right-position' or 'fh5co-center-position' to change the layout position
+								Example:
+								<div class="fh5co-right-position">
+							-->
+							<div class="fh5co-center-position">
+								<h2 class="animate-box">NOSSOS PARCEIROS FAZEM A DIFERENÇA</h2>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="fh5co-learn-more animate-box">
+					<a href="#" class="scroll-btn">
+						<span class="text">Veja nossos Parceiros</span>
+						<span class="arrow"><i class="icon-chevron-down"></i></span>
+					</a>
+				</div>
+			</section>
+			<!-- END #fh5co-hero -->
+
+			<section id="fh5co-features">
+				<div class="container">
+					<div class="row text-center row-bottom-padded-md">
+						<div class="col-md-8 col-md-offset-2">
+							
+							<h2 class="fh5co-lead animate-box">
+							Generosas corporações, fundações e investidores individuais.</h2>
+							
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<a href="https://www.leroymerlin.com.br/" target="__blank">
+									<img src="img/PARCEIROS/logo-leroy-merlin.png">	
+									</a>
+								</div>
+								<h3 class="animate-box">LEROY MERLIN </h3>
+								
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<img src="img/PARCEIROS/fumcad_logo.png">
+								</div>
+								<h3 class="animate-box">FUMCAD</h3>
+								
+							</div>
+						</div>
+						<div class="clearfix visible-sm-block"></div>
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<img src="img/PARCEIROS/meyer.jpg" style="max-height: 500px; max-width: 400px">	
+								</div>
+								<h3 class="animate-box">MACHADO MEYER</h3>
+								
+							</div>
+						</div>
+
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-paragraph"></i>
+								</div>
+								<h3 class="animate-box">INAFLEX</h3>
+								
+							</div>
+						</div>
+						<div class="clearfix visible-sm-block"></div>
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-umbrella"></i>
+								</div>
+								<h3 class="animate-box">COMEÇANHA ADVOGADOS ASSOCIADOS</h3>
+								
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-toggle"></i>
+								</div>
+								<h3 class="animate-box">LEGRAND</h3>
+								
+							</div>
+						</div>
+						<div class="clearfix visible-sm-block"></div>
+					</div>
+					<div class="row">
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-power"></i>
+								</div>
+								<h3 class="animate-box">SANTA ALDEIA</h3>
+								
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-flag2"></i>
+								</div>
+								<h3 class="animate-box">TCRE</h3>
+								
+							</div>
+						</div>
+						<div class="clearfix visible-sm-block"></div>
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-anchor"></i>
+								</div>
+								<h3 class="animate-box">TURIN</h3>
+								
+							</div>
+						</div>
+
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-paragraph"></i>
+								</div>
+								<h3 class="animate-box">SYMRISE</h3>
+								
+							</div>
+						</div>
+						<div class="clearfix visible-sm-block"></div>
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-umbrella"></i>
+								</div>
+								<h3 class="animate-box">PRATO CHEIO</h3>
+								
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-toggle"></i>
+								</div>
+								<h3 class="animate-box">AIR LIQUIDE</h3>
+								
+							</div>
+						</div>
+						<div class="clearfix visible-sm-block"></div>
+					</div>
+					<div class="row">
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-power"></i>
+								</div>
+								<h3 class="animate-box">BNP PARIBAS</h3>
+								
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-flag2"></i>
+								</div>
+								<h3 class="animate-box">MGFARTE</h3>
+								
+							</div>
+						</div>
+						<div class="clearfix visible-sm-block"></div>
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-anchor"></i>
+								</div>
+								<h3 class="animate-box">CASTOR HORTIFRUTI</h3>
+								
+							</div>
+						</div>
+
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-paragraph"></i>
+								</div>
+								<h3 class="animate-box">ITAIQUARA</h3>
+								
+							</div>
+						</div>
+						<div class="clearfix visible-sm-block"></div>
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-umbrella"></i>
+								</div>
+								<h3 class="animate-box">RÁSCAL</h3>
+								
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-toggle"></i>
+								</div>
+								<h3 class="animate-box">RE*DIAL</h3>
+								
+							</div>
+						</div>
+						<div class="clearfix visible-sm-block"></div>
+						
+					</div>
+					<div class="row">
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-power"></i>
+								</div>
+								<h3 class="animate-box">MCANIZARES</h3>
+								
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-flag2"></i>
+								</div>
+								<h3 class="animate-box">KNAUF INDUSTRIES </h3>
+								
+							</div>
+						</div>
+						<div class="clearfix visible-sm-block"></div>
+						<div class="col-md-4 col-sm-6 col-xs-12">
+							<div class="fh5co-feature">
+								<div class="fh5co-icon animate-box">
+									<i class="icon-anchor"></i>
+								</div>
+								<h3 class="animate-box">CONSIGAZ</h3>
+								
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</section>	
+
+			
+
+
+			
+			
+
+							
+					</div>
+				</div>
+			</section>
+			
+			<?php
+				require_once('build/footer.php');
+			?>
+
+	</body>
+</html>
+
